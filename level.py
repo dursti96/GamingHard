d11fc4934b880f448f24634a0c71eca56aa05820
dursti96 <86721188+dursti96@users.noreply.github.com>
2021-11-12 12:16:29 +0100

    Update main

--- level.py
+++
@@ -1,9 +0,0 @@
-import pygame as pg
-
-
-class Level(pg.surface.Surface):
-    def __init__(self, name, background):
-        super().__init__()
-        self.name = name
-        self.background = background
-
